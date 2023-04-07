24722c2d6257f7c6af834abf72051eea3e13340b
Fahad <fahad4201060@gmail.com>
2023-04-08 00:43:24 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Pricing</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
